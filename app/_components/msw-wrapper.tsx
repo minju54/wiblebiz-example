fb469eee5439d868889cbadf5e3d01ed62a8b287
고민주 <minju.ko@lotte.net>
2025-02-10 15:03:19 +0900

    feat: 개발 환경에서만 msw 적용하기 위한 설정 추가

--- app/_components/msw-wrapper.tsx
+++ app/_components/msw-wrapper.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 
 // MSW(Mock Service Worker)를 초기화하고, 초기화가 완료된 후에 자식 컴포넌트를 렌더링
 const MSWWrapper = ({ children }: { children: React.ReactNode }) => {
+  const isDev = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled';
+
   const [mswReady, setMswReady] = useState<boolean>(false);
 
   // 컴포넌트 마운트 시 비동기적으로 initMsw를 가져옴
@@ -22,6 +24,7 @@ const MSWWrapper = ({ children }: { children: React.ReactNode }) => {
     return null; // MSW가 초기화될 때까지 아무것도 렌더링하지 않음
   }
 
+  if (!isDev) return null;
   return <>{children}</>;
 };
 
